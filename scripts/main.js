a4c4b1f91e672286254e87e4f3ef06c0bce0a87a
ldipasquale <yo@lucianodipasquale.me>
2018-01-11 16:47:56 -0300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.jampp__Carousel').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    focusOnSelect: true,
+  })
+})
